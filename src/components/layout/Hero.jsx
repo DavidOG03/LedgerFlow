db2f8794bd8790f1f85cf4ec6d6bdca6e6ff3fc3
David Ogungbemi <107545464+DavidOG03@users.noreply.github.com>
2026-05-26 03:45:42 +0100

    Update Hero.jsx

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
           sizes="(max-width: 412px) 380px, 760px"
           fetchPriority="high"
           alt="grid pattern"
-          className="absolute scale-120 md:scale-100 -top-20 left-0 mix-blend-soft-light opacity-10 brightness-125 contrast-75 scale-1.2 dark:opacity-100 pointer-events-none z-[-1]"
+          className="absolute scale-120 md:scale-100 -top-20 left-0 mix-blend-soft-light opacity-10 brightness-75 filter contrast-75 scale-1.2 dark:opacity-100 pointer-events-none z-[-1]"
         />
         <img
           srcSet="grid-380.webp 380w, grid.webp 760w"
@@ -24,7 +24,7 @@ const Hero = () => {
           fetchPriority="high"
           decoding="async"
           alt="grid pattern"
-          className="absolute hidden md:block -top-20 right-0 mix-blend-soft-light opacity-10 brightness-125 contrast-75 scale-1.2 dark:opacity-100 pointer-events-none z-[-1]"
+          className="absolute hidden md:block -top-20 right-0 mix-blend-soft-light opacity-10 brightness-75 filter contrast-75 scale-1.2 dark:opacity-100 pointer-events-none z-[-1]"
         />
 
         <div className="flex flex-col gap-4 text-center relative z-10 mt-10 md:mt-20">
